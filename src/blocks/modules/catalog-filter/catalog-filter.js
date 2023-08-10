4d2e36b54aaf203deb8346498cd7ad56cbfd062c
Andrey Brekotkin <brekot@ya.ru>
2023-08-10 22:34:26 +0300

    Slide add

--- src/blocks/modules/catalog-filter/catalog-filter.js
+++ src/blocks/modules/catalog-filter/catalog-filter.js
@@ -3,5 +3,7 @@ document.querySelectorAll('.catalog-filter__name').forEach((e) => {
     e.addEventListener('click', (e) => {
 
         e.target.classList.toggle("catalog-filter__name_open");
+
+        slideToggle(e.target.nextElementSibling, 300);
     });
 });
---
+++ src/js/template.js
@@ -0,0 +1,54 @@
+if (document.querySelector('.js-index-active'))
+{
+    let blocks = document.querySelectorAll('.js-index-active'),
+        blockCount = blocks.length,
+        leftLine = document.querySelector('.nav-index-line__count-full');
+
+    if (leftLine) leftLine.innerText = (blockCount > 9 ? blockCount : '0' + blockCount);
+
+    function reverseBlocks()
+    {
+       // if (window.innerWidth < 992) return;
+
+        let scrollDistance = window.scrollY;
+
+        blocks.forEach((el, i) => {
+
+            if (el.offsetTop - 50 <= scrollDistance)
+            {
+                if (blocks[i].dataset.reverseHeader === 'Y')
+                {
+                    document.querySelector('.header-main').classList.add('header-main_reverse');
+                }
+                else
+                {
+                    document.querySelector('.header-main').classList.remove('header-main_reverse');
+                }
+            }
+
+            if (leftLine && el.offsetTop - (window.innerHeight / 2) <= scrollDistance)
+            {
+                if (scrollDistance == 0) i = 0;
+
+                if (blocks[i].dataset.reverseHeader === 'Y')
+                {
+                    document.querySelector('.nav-index-line').classList.add('nav-index-line_reverse');
+                }
+                else
+                {
+                    document.querySelector('.nav-index-line').classList.remove('nav-index-line_reverse');
+                }
+
+                let num = i + 1;
+
+                document.querySelector('.nav-index-line__count-this').innerText = (num > 9 ? num : '0' + num);
+
+                document.querySelector('.nav-index-line__name').innerText = blocks[i].dataset.name;
+            }
+        });
+    }
+
+    window.addEventListener('scroll', reverseBlocks);
+
+    reverseBlocks();
+}
